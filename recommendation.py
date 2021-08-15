31532cc59bc393f43de87c1ffdfd61e17a38afd3
namuhuchutong <namuhuchutong@gmail.com>
2021-08-15 19:16:27 +0900

    Initial commit

---
+++ recommendation.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import scipy.sparse as sp
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
